e80b6ec06b0b4dd785ed562d1b536b31023ecda3
Christian Weiske <cweiske@cweiske.de>
2022-08-18 15:59:47 +0200

    Add defold source code files

---
+++ src/com/dynamo/bob/archive/ArchiveEntry.java
@@ -0,0 +1,118 @@
+// Copyright 2020-2022 The Defold Foundation
+// Copyright 2014-2020 King
+// Copyright 2009-2014 Ragnar Svensson, Christian Murray
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+// 
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+package com.dynamo.bob.archive;
+
+import java.io.IOException;
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
+
+public class ArchiveEntry implements Comparable<ArchiveEntry> {
+    public static final int FLAG_ENCRYPTED = 1 << 0;
+    public static final int FLAG_COMPRESSED = 1 << 1;
+    public static final int FLAG_LIVEUPDATE = 1 << 2;
+    public static final int FLAG_UNCOMPRESSED = 0xFFFFFFFF;
+
+    // Member vars, TODO make these private and add getters/setters
+    public int size;
+    public int compressedSize;
+    public int resourceOffset;
+    public int flags = 0;
+    public String relName;
+    public String fileName;
+    public byte[] hash = null;
+
+    public ArchiveEntry(String fileName) throws IOException {
+        this.fileName = fileName;
+        this.relName = fileName;
+    }
+
+    public ArchiveEntry(String root, String fileName, boolean compress, boolean encrypt, boolean isLiveUpdate) throws IOException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            throw new IOException(String.format("File %s doens't exists",
+                    fileName));
+        }
+
+        fileName = file.getAbsolutePath();
+        if (!fileName.startsWith(root)) {
+            throw new IOException(String.format(
+                    "File %s isn't relative to root directory %s",
+                    fileName, root));
+        }
+
+        this.size = (int) file.length();
+        if(compress) {
+            // Will be set to real value after compression
+            this.compressedSize = 0;
+        } else {
+            this.compressedSize = FLAG_UNCOMPRESSED;
+        }
+
+        if (encrypt) {
+            this.flags = this.flags | FLAG_ENCRYPTED;
+        }
+
+        if (isLiveUpdate) {
+            this.flags = this.flags | FLAG_LIVEUPDATE;
+        }
+
+        this.relName = FilenameUtils.separatorsToUnix(fileName.substring(root.length()));
+        this.fileName = fileName;
+    }
+
+    public ArchiveEntry(String root, String fileName, boolean compress, boolean encrypt) throws IOException {
+        this(root, fileName, compress, encrypt, false);
+    }
+
+    // For checking duplicate when constructing archive
+    @Override
+    public boolean equals(Object other){
+        boolean result = this.getClass().equals(other.getClass());
+        if (result) {
+            ArchiveEntry entryOther = (ArchiveEntry)other;
+            result = this.fileName.equals(entryOther.fileName)
+                    && this.relName.equals(entryOther.relName)
+                    && this.flags == entryOther.flags;
+        }
+        return result;
+    }
+
+    public int hashCode()
+    {
+        return 17 * this.fileName.hashCode() + 31 * this.relName.hashCode();
+    }
+
+    private int compare(byte[] left, byte[] right) {
+        for (int i = 0, j = 0; i < left.length && j < right.length; i++, j++) {
+            int a = (left[i] & 0xff);
+            int b = (right[j] & 0xff);
+            if (a != b) {
+                return a - b;
+            }
+        }
+        return left.length - right.length;
+    }
+
+    // For sorting according to hash when building archive
+    @Override
+    public int compareTo(ArchiveEntry other) {
+        if (this.hash == null || other.hash == null) {
+            return this.relName.compareTo(other.relName);
+        }
+
+        return this.compare(this.hash, other.hash);
+    }
+}
---
+++ src/com/dynamo/bob/archive/ArchiveReader.java
@@ -0,0 +1,184 @@
+// Copyright 2020-2022 The Defold Foundation
+// Copyright 2014-2020 King
+// Copyright 2009-2014 Ragnar Svensson, Christian Murray
+// Licensed under the Defold License version 1.0 (the "License"); you may not use
+// this file except in compliance with the License.
+// 
+// You may obtain a copy of the License, together with FAQs at
+// https://www.defold.com/license
+// 
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+package com.dynamo.bob.archive;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dynamo.liveupdate.proto.Manifest.ManifestData;
+import com.dynamo.liveupdate.proto.Manifest.ManifestFile;
+import com.dynamo.liveupdate.proto.Manifest.ResourceEntry;
+
+public class ArchiveReader {
+    public static final int VERSION = 4;
+    public static final int HASH_BUFFER_BYTESIZE = 64; // 512 bits
+
+    private ArrayList<ArchiveEntry> entries = null;
+
+    private int entryCount = 0;
+    private int entryOffset = 0;
+    private int hashOffset = 0;
+    private int hashLength = 0;
+
+    private final String archiveIndexFilepath;
+    private final String archiveDataFilepath;
+    private final String manifestFilepath;
+    private RandomAccessFile archiveIndexFile = null;
+    private RandomAccessFile archiveDataFile = null;
+    private ManifestFile manifestFile = null;
+
+    public ArchiveReader(String archiveIndexFilepath, String archiveDataFilepath, String manifestFilepath) {
+        this.archiveIndexFilepath = archiveIndexFilepath;
+        this.archiveDataFilepath = archiveDataFilepath;
+        this.manifestFilepath = manifestFilepath;
+    }
+
+    public void read() throws IOException {
+    	this.archiveIndexFile = new RandomAccessFile(this.archiveIndexFilepath, "r");
+        this.archiveDataFile = new RandomAccessFile(this.archiveDataFilepath, "r");
+        
+        this.archiveIndexFile.seek(0);
+        this.archiveDataFile.seek(0);
+        
+        if (this.manifestFilepath != null) {
+	        InputStream manifestInputStream = new FileInputStream(this.manifestFilepath);
+	        this.manifestFile = ManifestFile.parseFrom(manifestInputStream);
+	        manifestInputStream.close();
+        }
+
+        // Version
+        int indexVersion = this.archiveIndexFile.readInt();
+        if (indexVersion == ArchiveReader.VERSION) {
+            readArchiveData();
+        } else {
+            throw new IOException("Unsupported archive index version: " + indexVersion);
+        }
+    }
+    
+    private boolean matchHash(byte[] a, byte[] b, int hlen) {
+    	if (a.length < hlen || b.length < hlen) {
+    		return false;
+    	}
+    	
+    	for (int i = 0; i < hlen; ++i) {
+    		if (a[i] != b[i]) {
+    			return false;
+    		}
+    	}
+    	
+    	return true;
+    }
+
+    private void readArchiveData() throws IOException {
+        // INDEX
+        archiveIndexFile.readInt(); // Pad
+        archiveIndexFile.readLong(); // UserData, should be 0
+        entryCount = archiveIndexFile.readInt();
+        entryOffset = archiveIndexFile.readInt();
+        hashOffset = archiveIndexFile.readInt();
+        hashLength = archiveIndexFile.readInt();
+
+        entries = new ArrayList<ArchiveEntry>(entryCount);
+
+        // Hashes are stored linearly in memory instead of within each entry, so the hashes are read in a separate loop.
+        // Once the hashes are read, the rest of the entries are read.
+
+        archiveIndexFile.seek(hashOffset);
+        // Read entry hashes
+        for (int i = 0; i < entryCount; ++i) {
+            archiveIndexFile.seek(hashOffset + i * HASH_BUFFER_BYTESIZE);
+            ArchiveEntry e = new ArchiveEntry("");
+            e.hash = new byte[HASH_BUFFER_BYTESIZE];
+            archiveIndexFile.read(e.hash, 0, hashLength);
+
+            if (this.manifestFile != null) {
+                ManifestData manifestData = ManifestData.parseFrom(this.manifestFile.getData());
+                for (ResourceEntry resource : manifestData.getResourcesList()) {
+	            	if (matchHash(e.hash, resource.getHash().getData().toByteArray(), this.hashLength)) {
+	            		e.fileName = resource.getUrl();
+	            		e.relName = resource.getUrl();
+	            	}
+	            }
+            }
+            
+            entries.add(e);
+        }
+
+        // Read entries
+        archiveIndexFile.seek(entryOffset);
+        for (int i=0; i<entryCount; ++i) {
+            ArchiveEntry e = entries.get(i);
+
+            e.resourceOffset = archiveIndexFile.readInt();
+            e.size = archiveIndexFile.readInt();
+            e.compressedSize = archiveIndexFile.readInt();
+            e.flags = archiveIndexFile.readInt();
+        }
+    }
+
+    public List<ArchiveEntry> getEntries() {
+        return entries;
+    }
+
+    public byte[] getEntryContent(ArchiveEntry entry) throws IOException {
+        byte[] buf = new byte[entry.size];
+        archiveDataFile.seek(entry.resourceOffset);
+        archiveDataFile.read(buf, 0, entry.size);
+
+        return buf;
+    }
+
+    public void extractAll(String path) throws IOException {
+
+        int entryCount = entries.size();
+
+        System.out.println("Extracting entries to " + path + ": ");
+        for (int i = 0; i < entryCount; i++) {
+            ArchiveEntry entry = entries.get(i);
+            String outdir = path + entry.fileName;
+            System.out.println("> " + entry.fileName);
+            int readSize = entry.compressedSize;
+
+            // extract
+            byte[] buf = new byte[entry.size];
+            archiveDataFile.seek(entry.resourceOffset);
+            archiveDataFile.read(buf, 0, readSize);
+
+            File fo = new File(outdir);
+            fo.getParentFile().mkdirs();
+            FileOutputStream os = new FileOutputStream(fo);
+            os.write(buf);
+            os.close();
+        }
+    }
+
+    public void close() throws IOException {
+        if (archiveIndexFile != null) {
+            archiveIndexFile.close();
+            archiveIndexFile = null;
+        }
+
+        if (archiveDataFile != null) {
+            archiveDataFile.close();
+            archiveDataFile = null;
+        }
+    }
+}
