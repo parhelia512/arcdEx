8d96ef195231c7a3971a5a2f03a9524e4961639a
Christian Weiske <cweiske@cweiske.de>
2022-08-20 11:51:08 +0200

    Add support for manifest v3

--- src/de/cweiske/arcdex/BetterArchiveReader.java
+++ src/de/cweiske/arcdex/BetterArchiveReader.java
@@ -3,10 +3,14 @@ package de.cweiske.arcdex;
 import com.dynamo.bob.archive.ArchiveEntry;
 import com.dynamo.bob.archive.ArchiveReaderProtected;
 import com.dynamo.crypt.Crypt;
+import com.dynamo.liveupdate.proto.Manifest;
+import com.dynamo.liveupdate.proto.Manifest3;
+import com.google.protobuf.InvalidProtocolBufferException;
 import net.jpountz.lz4.LZ4Factory;
 import net.jpountz.lz4.LZ4FastDecompressor;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Defold .arcd archive reader with support for compression and decryption
@@ -18,11 +22,119 @@ public class BetterArchiveReader extends ArchiveReaderProtected {
      */
     protected final byte[] KEY = "aQj8CScgNP4VsfXK".getBytes();
     protected LZ4FastDecompressor decompressor;
+
+    protected Manifest.ManifestData manifestv4;
+    protected Manifest3.ManifestData manifestv3;
+
     public BetterArchiveReader(String archiveIndexFilepath, String archiveDataFilepath, String manifestFilepath) {
         super(archiveIndexFilepath, archiveDataFilepath, manifestFilepath);
         LZ4Factory factory = LZ4Factory.fastestInstance();
         decompressor = factory.fastDecompressor();
     }
+
+    /**
+     * Handle both manifest v4 and v3
+     */
+    protected void readArchiveData() throws IOException {
+        // INDEX
+        archiveIndexFile.readInt(); // Pad
+        archiveIndexFile.readLong(); // UserData, should be 0
+        entryCount = archiveIndexFile.readInt();
+        entryOffset = archiveIndexFile.readInt();
+        hashOffset = archiveIndexFile.readInt();
+        hashLength = archiveIndexFile.readInt();
+
+        entries = new ArrayList<ArchiveEntry>(entryCount);
+
+        // Hashes are stored linearly in memory instead of within each entry, so the hashes are read in a separate loop.
+        // Once the hashes are read, the rest of the entries are read.
+
+        archiveIndexFile.seek(hashOffset);
+        loadManifest();
+        // Read entry hashes
+        for (int i = 0; i < entryCount; ++i) {
+            archiveIndexFile.seek(hashOffset + i * HASH_BUFFER_BYTESIZE);
+            ArchiveEntry e = new ArchiveEntry("");
+            e.hash = new byte[HASH_BUFFER_BYTESIZE];
+            archiveIndexFile.read(e.hash, 0, hashLength);
+
+            if (this.manifestv4 != null) {
+                for (Manifest.ResourceEntry resource : manifestv4.getResourcesList()) {
+                    if (matchHash(e.hash, resource.getHash().getData().toByteArray(), this.hashLength)) {
+                        e.fileName = resource.getUrl();
+                        e.relName = resource.getUrl();
+                    }
+                }
+
+            } else if (this.manifestv3 != null) {
+                for (Manifest3.ResourceEntry resource : manifestv3.getResourcesList()) {
+                    if (matchHash(e.hash, resource.getHash().getData().toByteArray(), this.hashLength)) {
+                        e.fileName = resource.getUrl();
+                        e.relName = resource.getUrl();
+                    }
+                }
+            }
+
+            entries.add(e);
+        }
+
+        // Read entries
+        archiveIndexFile.seek(entryOffset);
+        for (int i=0; i<entryCount; ++i) {
+            ArchiveEntry e = entries.get(i);
+
+            e.resourceOffset = archiveIndexFile.readInt();
+            e.size = archiveIndexFile.readInt();
+            e.compressedSize = archiveIndexFile.readInt();
+            e.flags = archiveIndexFile.readInt();
+        }
+    }
+
+    /**
+     * Load manifest 4, fall back to manifest v3 loading
+     *
+     * v3: https://github.com/defold/defold/commit/17c765d7dcc970b1be616e40449e1f3905263d24
+     * v4: https://github.com/defold/defold/commit/4eebea163ced71074f10761ea7774bb2b2b38faf
+     */
+    protected void loadManifest() throws InvalidProtocolBufferException {
+        int version;
+
+        try {
+            this.manifestv4 = Manifest.ManifestData.parser().parsePartialFrom(this.manifestFile.getData());
+            //ManifestData manifestData = ManifestData.parseFrom(this.manifestFile.getData());
+            return;
+
+        } catch (com.google.protobuf.InvalidProtocolBufferException exception) {
+            //could not parse manifest file
+            Manifest.ManifestData brokenManifestData = (Manifest.ManifestData) exception.getUnfinishedMessage();
+
+            if (!brokenManifestData.hasHeader()) {
+                System.err.println("Error: Could not parse manifest. Header missing.");
+                System.exit(10);
+            }
+
+            if (!brokenManifestData.getHeader().hasVersion()) {
+                System.err.println("Error: Could not parse manifest. Version in header missing.");
+                System.exit(10);
+            }
+
+            version = brokenManifestData.getHeader().getVersion();
+            if (version == 4) {
+                System.err.println("Error: Could not parse manifest v4. Probably damaged.");
+                System.exit(10);
+            }
+        }
+
+        if (version == 3) {
+            //load manifest v3
+            this.manifestv3 = Manifest3.ManifestData.parseFrom(this.manifestFile.getData());
+            return;
+        }
+
+        System.err.println("Error: Cannot parse manifest v" + version);
+        System.exit(10);
+    }
+
 
     public byte[] getDecompressedEntryContent(ArchiveEntry entry) throws IOException {
         archiveDataFile.seek(entry.resourceOffset);
